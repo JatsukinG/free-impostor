8467f3a64174e3c8bb8d98e31220cbc42bd34b80
JatsukinG <marketplacepremiumdev@gmail.com>
2026-01-25 21:44:31 -0500

    Logo

--- src/modules/home/Welcome.tsx
+++ src/modules/home/Welcome.tsx
@@ -5,11 +5,14 @@ const Welcome = () => {
   const { initGame } = useGame()
   return (
       <div className="w-full flex flex-col items-center px-8">
-        <img
-            alt="detective"
-            src="/impostor.png"
-            className="my-8 text-red-400 w-full"
-        />
+        <h1 className="my-12 text-4xl uppercase font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-violet-800 to-purple-400">
+          Impostor
+        </h1>
+        {/*<img*/}
+        {/*    alt="detective"*/}
+        {/*    src="/impostor.png"*/}
+        {/*    className="my-8 text-red-400 w-full"*/}
+        {/*/>*/}
         <NiceButton onClick={() => initGame()}>
           ¡Comenzar Juego!
         </NiceButton>
